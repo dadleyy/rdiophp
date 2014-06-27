9efd5a7e209a52aea206af61c0982e3efc3a9bf4
Danny Hadley <danny@dadleyy.com>
2014-06-27 17:03:22 -0400

    adding some things into source to let travis be happy

--- tests/ApiClientTest.php
+++ tests/ApiClientTest.php
@@ -5,7 +5,7 @@ use Dadleyy\Rdio\ApiClient;
 class ApiClientTest extends PHPUnit_Framework_TestCase {
 
   public function testActivity() {
-    $client = new ApiClient();
+    $client = new ApiClient(getenv('RDIO_CLIENT'), getenv('RDIO_SECRET'));
     $this->assertTrue(is_array($client->activity('s8297079')));
   }
 
